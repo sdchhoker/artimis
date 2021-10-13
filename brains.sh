15e0325767cc425e2a53e60c5ca35c346c17dad6
Sandeep Chhoker <schhoker@reputation.com>
2021-10-13 13:56:44 +0530

    updated code for bash script

--- brains.sh
+++ brains.sh
@@ -1,10 +1,12 @@
 echo "script started"
-for ((i=1;i<=10;i++))
+for ((i=1;i<=100;i++))
 do
   echo "open links"
   open https://www.youtube.com/watch?v=8rYGbB_nGjY&t=6s
   sleep 40s
-  open https://www.youtube.com/watch?v=i-lSSeXOnUc
+  open https://www.youtube.com/watch?v=_4k64HyGcQA
+  sleep 25s
+  open https://www.youtube.com/watch?v=4HO40FmQJxs
   sleep 25s
   open https://www.youtube.com/watch?v=4HO40FmQJxs
   sleep 25s
@@ -16,7 +18,7 @@ do
   sleep 25s
   open https://www.youtube.com/watch?v=9BltCJw7G3E
   echo "sleep to close tabs"
-  sleep 280s
+  sleep 220s
   echo "close browser"
   osascript -e 'quit app "Safari"'
   sleep 25s
